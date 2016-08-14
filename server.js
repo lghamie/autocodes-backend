d037a495045f4ed2ef7c31db98bbae5e85d05fec
lghamie <lucas.hamie@gmail.com>
2016-08-14 11:11:16 -0300

    Modificado ID de usuario que se usa para la busqueda

--- server.js
+++ server.js
@@ -191,7 +191,7 @@ app.get('/groups/:userid', function(req, res) {
         return;
     }
     pool.query('SELECT `group`.group_id, `group`.name FROM `group` INNER JOIN group_user ON `group`.group_id = group_user.group_id INNER JOIN user ON user.user_id = group_user.user_id ' +
-        ' WHERE user.user_id = ?', userid,
+        ' WHERE user.user_fb_id = ?', userid,
         function(err, rows, fields) {
             if (err) {
                 res.send({ error: err });
